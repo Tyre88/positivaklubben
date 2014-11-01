948521fdacdcc64c9442e666dbf36fd11b61cadb
Tyre <victor@webbdudes.se>
2014-11-01 14:03:24 +0100

    Testing the code

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,24 @@ app.controller('home', ['$scope', '$http', function($scope, $http)
 {
 	$scope.Tasks = [];
 
+	var task = {
+		name: "Tapetsera",
+		description: "Tapetsera övanvåningen",
+		createdby: "Anton Winblad",
+		due: "2014-11-12",
+		starttime: "09:00",
+		endtime: "20:00",
+		comments: [{
+					   content: "AWSOME, jag kommer!",
+					   createdby: "Victor Öhrström"
+				   }]
+	};
+
+	//$http.post('http://localhost:8000/task', task).success(function(data)
+	//{
+	//	alert('done');
+	//});
+
 	$http.get('http://localhost:8000/tasks').success(function(data)
 	{
 		$scope.Tasks = data;
